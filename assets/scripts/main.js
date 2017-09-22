b5ade483a525ba25f9db05dc94f09ffda1e4c1a9
Adil Heybetov <hadil.p101@code.edu.az>
2017-09-22 17:06:06 +0400

    all files uploaded

---
+++ assets/scripts/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:0,
+    responsiveClass:true,
+    items:1,
+    nav:true
+    // responsive:{
+    //     0:{
+    //     },
+    //     600:{
+    //     },
+    //     1000:{
+    //     }
+    // }
+})
+})
